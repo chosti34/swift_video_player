23664221c96e06c5cb92af39ccea208aad72dd24
Timur Karimov <chosti34@gmail.com>
2020-08-06 19:47:23 +0300

    Инициализация проекта

---
+++ video_player/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Тимур Каримов on 06/08/2020.
+//  Copyright © 2020 Тимур Каримов. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
